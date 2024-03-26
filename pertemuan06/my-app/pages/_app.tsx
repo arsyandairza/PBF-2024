2002b458b782a133c91995bbe55bcce6fa81d35c
arsyandaIrza <arsyandairza123@gmail.com>
2024-03-26 12:13:29 +0700

    W06: Praktikum 3

--- pertemuan06/my-app/pages/_app.tsx
+++ pertemuan06/my-app/pages/_app.tsx
@@ -2,17 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
+import { store } from '../redux/store/store';
 import { Provider } from "react-redux";
-import { store } from "@/redux/store/store.js";
 
-function MyApp({Component, pageProps}:any) {
+function MyApp({Component, pageProps}:any){
   useEffect(()=>{
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   },[]);
 
   return (
     <Provider store={store}>
-      <Component {...pageProps}/>
+      <Component {...pageProps}/>;
     </Provider>
   )
 }
